10b5204812bbac0fcfc1414a7c1c314c3daedd39
david <datunebi2003@gmail.com>
2025-12-03 00:13:59 +0200

    Prisma ORM without Rust engines

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,4 @@
 import type {NextConfig} from "next";
-
-const {PrismaPlugin} = require('@prisma/nextjs-monorepo-workaround-plugin')
-
-const nextConfig: NextConfig = {
-	webpack: (config, {isServer}) => {
-		if (isServer) {
-			config.plugins = [...config.plugins, new PrismaPlugin()]
-		}
-		
-		return config
-	},
-	turbopack: {}
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
